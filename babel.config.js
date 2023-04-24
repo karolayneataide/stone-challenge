64f916d282f9d6d5d7e7f7719ee82299f2e6a0ad
Karolayne Ataíde <karolayne.o.v.a@gmail.com>
2023-04-24 00:38:39 -0300

    chore: add babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,18 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@ions': './src/components/ions/index.ts',
+            '@atoms': './src/components/atoms/index.ts',
+            '@molecules': './src/components/molecules/index.ts',
+            '@organisms': './src/components/organisms/index.ts',
+          },
+        },
+      ],
+    ],
   };
 };
