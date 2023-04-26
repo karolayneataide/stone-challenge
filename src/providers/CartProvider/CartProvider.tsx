3afb4d962c79eba59d5377448d970ab0937d1b21
Karolayne Ataíde <karolayne.o.v.a@gmail.com>
2023-04-26 04:01:20 -0300

    chore: add types in CartProvider

--- src/providers/CartProvider/CartProvider.tsx
+++ src/providers/CartProvider/CartProvider.tsx
@@ -1,7 +1,14 @@
 import { Product } from '@types';
 import React, { PropsWithChildren, createContext, useState } from 'react';
 
-export const CartContext = createContext({});
+interface CartProps {
+  cart: Array<Product>;
+  addProductToCart: (product: Product) => void;
+  removeProductFromCart: (id: number) => void;
+  hasProductInCart: (id: number) => boolean;
+}
+
+export const CartContext = createContext({} as CartProps);
 
 export const CartProvider = ({ children }: PropsWithChildren) => {
   const [cart, setCart] = useState<Array<Product>>([]);
@@ -11,13 +18,22 @@ export const CartProvider = ({ children }: PropsWithChildren) => {
   }
 
   function removeProductFromCart(id: number) {
-    const filteredCart = cart.filter((cartItem) => cartItem.id === id);
+    const filteredCart = cart.filter((cartItem) => cartItem.id !== id);
     setCart(filteredCart);
+  }
+
+  function hasProductInCart(id: number) {
+    return Boolean(cart.find((item) => item.id === id));
   }
 
   return (
     <CartContext.Provider
-      value={{ cart, addProductToCart, removeProductFromCart }}
+      value={{
+        cart,
+        addProductToCart,
+        removeProductFromCart,
+        hasProductInCart,
+      }}
     >
       {children}
     </CartContext.Provider>
