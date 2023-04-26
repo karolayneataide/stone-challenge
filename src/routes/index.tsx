970e475ce4db241f83be328b05dc870ff8d7a3ba
Karolayne Ataíde <karolayne.o.v.a@gmail.com>
2023-04-26 04:01:35 -0300

    chore: add styles in header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { CartButton } from '@ions';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Cart, Products } from '@screens';
+import theme from '@theme';
 import React from 'react';
 
 const Stack = createStackNavigator();
@@ -13,7 +14,14 @@ export const NavigationProvider = () => {
       <Stack.Navigator
         screenOptions={({ navigation }) => ({
           headerTitleAlign: 'center',
-          headerRight: () => <CartButton navigation={navigation} />,
+          headerRight: ({ tintColor }) => (
+            <CartButton navigation={navigation} tintColor={tintColor} />
+          ),
+          headerShadowVisible: false,
+          headerTintColor: theme.palette.common.white,
+          headerStyle: {
+            backgroundColor: theme.palette.primary[400],
+          },
         })}
       >
         <Stack.Screen name="Produtos" component={Products} />
