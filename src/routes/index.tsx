447d7d0e314886643eafc3ea95c0f84345e4e218
Karolayne Ataíde <karolayne.o.v.a@gmail.com>
2023-04-26 00:31:54 -0300

    feat: add cart button in header menu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { CartButton } from '@ions';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Cart, Products } from '@screens';
@@ -9,9 +11,10 @@ export const NavigationProvider = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{
+        screenOptions={({ navigation }) => ({
           headerTitleAlign: 'center',
-        }}
+          headerRight: () => <CartButton navigation={navigation} />,
+        })}
       >
         <Stack.Screen name="Produtos" component={Products} />
         <Stack.Screen name="Meu carrinho" component={Cart} />
