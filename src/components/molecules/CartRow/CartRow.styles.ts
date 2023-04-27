65912c9c37e2e5199a5eb35e38daffdbf96d9ab3
Karolayne Ataide <karolayne.o.v.a@gmail.com>
2023-04-27 02:33:32 -0300

    fix: remove elevation and add border

--- src/components/molecules/CartRow/CartRow.styles.ts
+++ src/components/molecules/CartRow/CartRow.styles.ts
@@ -4,12 +4,14 @@ export const Wrapper = styled.View`
   height: 130px;
   margin: ${({ theme }) => theme.spacing[4]};
   border-radius: ${({ theme }) => theme.spacing[3]};
-  elevation: 3;
+  border-width: ${({ theme }) => theme.spacing[1]};
+  border-color: ${({ theme }) => theme.palette.gray[100]};
   flex-direction: row;
 `;
 
 export const ImageWrapper = styled.View`
   width: 130px;
+  border-radius: ${({ theme }) => theme.spacing[3]};
 `;
 
 export const Image = styled.Image`
